48816e6219c868f4f0e7827e3a321cd9ede298c3
thiepn <junsosmart@gmail.com>
2026-09-11 18:12:22 +0200

    test: configure phone screen through context options

--- playwright.config.ts
+++ playwright.config.ts
@@ -55,7 +55,7 @@ export default defineConfig({
       use: {
         browserName: 'chromium',
         viewport: { width: 390, height: 844 },
-        screen: { width: 390, height: 844 },
+        contextOptions: { screen: { width: 390, height: 844 } },
         deviceScaleFactor: 2,
         hasTouch: true,
         isMobile: true,
@@ -66,7 +66,7 @@ export default defineConfig({
       use: {
         browserName: 'webkit',
         viewport: { width: 390, height: 844 },
-        screen: { width: 390, height: 844 },
+        contextOptions: { screen: { width: 390, height: 844 } },
         deviceScaleFactor: 3,
         hasTouch: true,
         isMobile: true,
